6eff2f31bf03df607d6f230f0ae3e284d22c518e
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-30 20:25:05 +0300

    adding comments to the getNum api

--- laravel-project/app/Http/Controllers/separateNumController.php
+++ laravel-project/app/Http/Controllers/separateNumController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class separateNumController extends Controller
 {
     function getNum($num){
-
+        //count the times that a number is divided by 10 
         $counter = 1;
+        // a list that will take all the values of numbers(Hundreds,tens,ones) in it at the end 
         $list = [] ;
         
+        // The array will take all the values of the user in a placevalue form 
         while ($num !=0 ){
             $rem = $num % 10 ;
             $list[] = ($rem * $counter);
@@ -18,7 +20,9 @@ class separateNumController extends Controller
             $num = round($num, 0);
             $counter = $counter * 10;
         }
-    
+        //reversing the values inside the list  
+        $list = array_reverse($list);
+        // returns json object
         return response()->json([
             "status" => "Success",
             "message" => $list
