35cd462d855d22d2c8c56cf2747c9213fabf406b
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-30 21:16:17 +0300

    adding the route that takes a string and call the method convertNum

--- laravel-project/routes/api.php
+++ laravel-project/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => "v1"], function(){
 
     Route::get('/numbers/{num}',[separateNumController::class,'getNum']);
-    
+    Route::post('/strings',[humanToprogrammerController::class,'convertNum']);
 
 });
 
